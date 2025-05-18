393b99047fce1ba7ba57f2746ec059d9f83324b1
BabiSato <barbaranayumisato@gmail.com>
2025-05-17 22:08:44 -0300

    first commit

---
+++ app/(auth)/register.tsx
@@ -0,0 +1,5 @@
+import { Register } from "@/src/screens/user/cadastro";
+
+export default function RegisterScreen() {
+  return (<Register/>)
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,44 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from 'expo-font';
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+import { SplashScreen } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+// Prevent splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
+    'Inter-Regular': require('@/assets/fonts/Inter_24pt-Regular.ttf'),
+    'Inter-Medium': require('@/assets/fonts/Inter_24pt-Medium.ttf'),
+    'Inter-Bold': require('@/assets/fonts/Inter_24pt-Bold.ttf'),
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
